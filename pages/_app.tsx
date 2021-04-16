9d8a1e167272211698f386f712b31fa11ff0e0d6
Entreprenerd <valeranijr@gmail.com>
2021-04-16 16:59:23 +0200

    feat: compute signatures and then prove them

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
 // import App from "next/app";
 import type { AppProps /* , AppContext */ } from "next/app";
+import { Web3ReactProvider } from "@web3-react/core";
+import { Provider, Web3Provider } from "@ethersproject/providers";
+import { UserContextProvider } from "../context/UserContext";
+
+const getLibrary = (provider: Provider): Web3Provider => {
+  return new Web3Provider(provider as any); // this will vary according to whether you use e.g. ethers or web3.js
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <UserContextProvider>
+        <Component {...pageProps} />;
+      </UserContextProvider>
+    </Web3ReactProvider>
+  );
 }
 
 // Only uncomment this method if you have blocking data requirements for
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,63 @@
+import { SigningKey } from "@ethersproject/signing-key";
 import Head from "next/head";
+import { useMemo, useState } from "react";
+import aes256 from "aes256";
+import { useLogin, useUser } from "../context/UserContext";
 import styles from "../styles/Home.module.css";
 
 const Home: React.FC = () => {
+  const user = useUser();
+  const login = useLogin();
+  const [pkA, setPkA] = useState<string | null>(null);
+  const [pkB, setPkB] = useState<string | null>(null);
+
+  const message = "Hey B, how are you?";
+
+  const signingKeyA = useMemo(() => {
+    return pkA ? new SigningKey(pkA) : null;
+  }, [pkA]);
+
+  const signingKeyB = useMemo(() => {
+    return pkB ? new SigningKey(pkB) : null;
+  }, [pkB]);
+
+  const sharedSignKey = useMemo(() => {
+    return signingKeyB
+      ? signingKeyB.computeSharedSecret(signingKeyA.publicKey) // Equivalent to signingKeyA.computeSharedSecret(signingKeyB.publicKey)
+      : null;
+  }, [signingKeyA, signingKeyB]);
+
+  const signedByA = useMemo(() => {
+    return sharedSignKey ? aes256.encrypt(sharedSignKey, message) : null;
+  }, [sharedSignKey]);
+
+  const readByB = useMemo(() => {
+    return signedByA ? aes256.decrypt(sharedSignKey, signedByA) : null;
+  }, [sharedSignKey, signedByA]);
+
+  const firstSignature = async () => {
+    const signer = user.provider.getSigner();
+    const sign = await signer.signMessage("SignA");
+    setPkA(sign);
+  };
+
+  const secondSignature = async () => {
+    const signer = user.provider.getSigner();
+    const sign = await signer.signMessage("SignB");
+    setPkB(sign);
+  };
+
+  if (!user) {
+    return (
+      <div>
+        Login Please
+        <button type="button" onClick={(e) => login()}>
+          Login
+        </button>
+      </div>
+    );
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -9,57 +65,51 @@ const Home: React.FC = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <div>
+        <h2>Using Signatures as secret keys</h2>
+        <pre>{pkA}</pre>
+        <button type="button" onClick={firstSignature}>
+          First Signature
+        </button>
+        <pre>{pkB}</pre>
+        <button type="button" onClick={secondSignature}>
+          Second Signature
+        </button>
 
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        {signingKeyA && signingKeyB && (
+          <div>
+            <h2>Key A</h2>
+            <h3>Public Key</h3>
+            <pre>{signingKeyA.publicKey}</pre>
+            <h3>Private Key</h3>
+            <pre>{signingKeyA.privateKey}</pre>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+            <h2>Key B</h2>
+            <h3>Public Key</h3>
+            <pre>{signingKeyB.publicKey}</pre>
+            <h3>Private Key</h3>
+            <pre>{signingKeyB.privateKey}</pre>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+            <h2>Shared Secret</h2>
+            <h3>From A</h3>
+            <pre>{signingKeyA.computeSharedSecret(signingKeyB.publicKey)}</pre>
+            <h3>From B</h3>
+            <pre>{signingKeyB.computeSharedSecret(signingKeyA.publicKey)}</pre>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+            <h2>Shared sign key</h2>
+            <pre>{sharedSignKey}</pre>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+            <h2>A sends a message to B</h2>
+            <pre>{message}</pre>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+            <h2>A signs it with AES256 with shared secret Version</h2>
+            <pre>{signedByA}</pre>
+
+            <h2>B Reads it with AES256 with shared secret</h2>
+            <pre>{readByB}</pre>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
